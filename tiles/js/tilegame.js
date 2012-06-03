a79a7b72e1a6b8b982b9b949c45973516b352bdc
Gilmore Davidson <gilmoreorless@gmail.com>
2012-06-03 21:47:44 +1000

    Added "sliding" functions and move tracking

--- tiles/js/tilegame.js
+++ tiles/js/tilegame.js
@@ -16,11 +16,16 @@
 		}
 		return array;
 	}
+
+
 	/*** Main game ***/
 
 	var Tiler = window.Tiler = function (canvas, options) {
 		if (typeof canvas === 'string') {
 			canvas = document.getElementById(canvas);
+		}
+		if (!canvas) {
+			throw new TypeError('Invalid canvas argument passed to Tiler');
 		}
 		this.game = {
 			canvas: canvas,
@@ -33,26 +38,39 @@
 		var sourceCanvas = document.createElement('canvas');
 		this.source = {
 			canvas: sourceCanvas,
-			context: sourceCanvas.getContext('2d')
+			context: sourceCanvas.getContext('2d'),
+			image: null
 		};
 		sourceCanvas.width = canvas.width;
 		sourceCanvas.height = canvas.height;
 
+		// TEMP for debugging
 		document.body.appendChild(sourceCanvas);
 
+		// Register click handler
+		canvas.addEventListener('click', this.clickHandler.bind(this), false);
+
+		// Work out pieces
 		this.setup();
 	};
 
 	var Tproto = Tiler.prototype;
 
 	Tproto.setup = function () {
-		this._valid = false;
 		var opts = this.options;
 		var rows = opts.rows = opts.rows || 3;
 		var cols = opts.cols = opts.cols || 3;
 		var length = rows * cols;
 		var colWidth = opts.colWidth = opts.width / cols;
 		var rowHeight = opts.rowHeight = opts.height / rows;
+
+		// Game state flags
+		this.state = {
+			valid: false,
+			finished: false,
+			moves: 0,
+			emptyIndex: -1
+		};
 
 		// Build the list of pieces to move
 		this.pieces = new Array(length);
@@ -78,11 +96,12 @@
 		shuffle(this.pieces);
 		// Restore the empty piece
 		this.pieces.push(last);
+		this.state.emptyIndex = length - 1;
 	};
 
 	Tproto.setSrc = function (src) {
 		var self = this;
-		function draw() {
+		function drawSource() {
 			self.source.context.drawImage(src, 0, 0);
 			self.render();
 		}
@@ -91,17 +110,17 @@
 		if (typeof src === 'string') {
 			if (/\.(?:png|gif|jpe?g)$/i.test(src)) {
 				var img = new Image();
-				img.onload = draw;
+				img.onload = drawSource;
 				img.src = src;
 				src = img;
 				drawn = true;
 			}
 		}
 		if (!drawn) {
-			draw();
+			drawSource();
 		}
-		this.src = src;
-		this._valid = true;
+		this.source.image = src;
+		this.state.valid = true;
 	};
 
 	Tproto.render = function () {
@@ -140,4 +159,55 @@
 			}
 		}
 	};
+
+	Tproto.clickHandler = function (e) {
+		var x = e.offsetX;
+		var y = e.offsetY;
+		var col = ~~(x / this.options.colWidth);
+		var row = ~~(y / this.options.rowHeight);
+		var index = row * this.options.rows + col;
+		if (!this.state.finished) {
+			this.moveTile(index);
+		}
+	};
+
+	Tproto.moveTile = function (index) {
+		var state = this.state;
+		var pieces = this.pieces;
+		var empty = state.emptyIndex;
+		var rows = this.options.rows;
+		var cols = this.options.cols;
+		if (
+			index === empty - 1 ||
+			index === empty + 1 ||
+			index === empty - cols ||
+			index === empty + cols
+		) {
+			// Valid move, let's do this thing
+			var temp = pieces[index];
+			pieces[index] = pieces[empty];
+			pieces[empty] = temp;
+			state.emptyIndex = index;
+			state.moves++;
+			this.render();
+			this.checkFinished();
+		}
+	};
+
+	Tproto.checkFinished = function () {
+		if (this.state.finished) {
+			return true;
+		}
+		var i = this.pieces.length;
+		while (i--) {
+			if (this.pieces[i].index !== i) {
+				return false;
+			}
+		}
+		// Game just finished
+		alert('Finished in ' + this.state.moves + ' moves!');
+		this.state.finished = true;
+		return true;
+	};
+
 })(this);
