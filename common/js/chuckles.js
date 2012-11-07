b22df8a5c6ef0bd246beac4e07135649fd2280e0
Gilmore Davidson <gilmoreorless@gmail.com>
2012-11-08 08:23:51 +1100

    New dragging mode for Chuckles to define where segment should be placed

--- common/js/chuckles.js
+++ common/js/chuckles.js
@@ -87,14 +87,15 @@ Chuckles = (function () {
         }
     };
 
-    cproto._drawSegment = function () {
-        if (this.segmentPath.length < 2) {
+    cproto._drawSegment = function (offset) {
+        var path = this.segmentPath;
+        if (path.length < 2) {
             return;
         }
         var ctx = this.ctx;
         // Clear the relative part of the bg image
         ctx.save();
-        this._drawPath(this.segmentPath);
+        this._drawPath(path);
         ctx.clip();
         if (this.fillStyle === 'none') {
             ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -105,7 +106,7 @@ Chuckles = (function () {
         ctx.restore();
         // Draw the segment part at the right offset
         ctx.save();
-        this._drawPath(this.segmentPath, this.getSegmentOffset());
+        this._drawPath(path, offset || this.getSegmentOffset());
         ctx.clip();
         this._drawBackground();
         ctx.restore();
@@ -133,11 +134,7 @@ Chuckles = (function () {
     };
 
     function addListener(elem, type, handler) {
-        if (elem.addEventListener) {
-            elem.addEventListener(type, handler, false);
-        } else if (elem.attachEvent) {
-            elem.attachEvent('on' + type, handler);
-        }
+        elem.addEventListener(type, handler, false);
         return {
             remove: function () {
                 removeListener(elem, type, handler);
@@ -146,19 +143,11 @@ Chuckles = (function () {
     }
 
     function removeListener(elem, type, handler) {
-        if (elem.removeEventListener) {
-            elem.removeEventListener(type, handler, false);
-        } else if (elem.detachEvent) {
-            elem.detachEvent('on' + type, handler);
-        }
+        elem.removeEventListener(type, handler, false);
     }
 
     function preventDefault(e) {
-        if (e.preventDefault) {
-            e.preventDefault();
-        } else {
-            e.returnValue = false;
-        }
+        e.preventDefault();
     }
 
     function getXY(e) {
@@ -187,23 +176,72 @@ Chuckles = (function () {
             this._listeners.selectStart.remove();
             // TODO: Use Simplify.js to reduce _points, then use that to make a new _tmpPath
             this._tmpPath.push(['closePath']);
-            drawMode.render(this);
+            drawMode.render(this, true);
             this.setSegmentPath(this._tmpPath);
             delete this._points;
             delete this._tmpPath;
         },
-        render: function (chuck) {
+        render: function (chuck, useDash) {
             chuck.render();
             chuck.ctx.save();
             chuck.ctx.strokeStyle = '#000';
+            if (useDash) {
+                chuck.ctx.setLineDash([3, 3]);
+            }
             chuck._drawPath(chuck._tmpPath);
             chuck.ctx.stroke();
+            if (useDash) {
+                chuck.ctx.strokeStyle = 'rgba(255, 255, 255, 0.5)';
+                chuck.ctx.lineDashOffset = 3;
+                chuck.ctx.stroke();
+            }
             chuck.ctx.restore();
+        }
+    };
+
+    var dragMode = {
+        start: function (e) {
+            var xy = getXY(e);
+            if (this.ctx.isPointInPath(xy.x, xy.y)) {
+                this._dragging = true;
+                this.movement = {x: 0, y: 0};
+                this._dragPointStart = xy;
+                this._dragPointOffset = {x: 0, y: 0};
+                this._oldCursor = this.canvas.style.cursor;
+                this.canvas.style.cursor = 'move';
+                this._listeners.dragModeDrag = addListener(this.canvas, 'mousemove', dragMode.drag.bind(this));
+                this._listeners.selectStart = addListener(document, 'selectstart', preventDefault);
+            }
+        },
+        drag: function (e) {
+            var xy = getXY(e);
+            this._dragPointOffset.x = xy.x - this._dragPointStart.x;
+            this._dragPointOffset.y = xy.y - this._dragPointStart.y;
+            this._drawBackground();
+            this._drawSegment(this._dragPointOffset);
+        },
+        end: function (e) {
+            if (this._dragging) {
+                this._listeners.dragModeDrag.remove();
+                this._listeners.selectStart.remove();
+                this.canvas.style.cursor = this._oldCursor;
+                this.movement = this._dragPointOffset;
+                delete this._dragPointStart;
+                delete this._dragPointOffset;
+                delete this._oldCursor;
+                delete this._dragging;
+                this.setPosition(1);
+            }
         }
     };
 
     var validModes = {normal: 1, drawing: 1, dragging: 1};
     var modeEvents = {
+        normal: {
+            setup: function () {
+                this.render();
+            }
+        },
         drawing: {
             setup: function () {
                 this.setSegmentPath([]);
@@ -219,10 +257,18 @@ Chuckles = (function () {
         },
         dragging: {
             setup: function () {
-                console.log('setup drawing');
+                this.setPosition(0);
+                this.ctx.save();
+                this.ctx.setLineDash([3, 3]);
+                this.ctx.strokeStyle = '#000';
+                this.ctx.stroke();
+                this.ctx.restore();
+                this._listeners.dragModeStart = addListener(this.canvas, 'mousedown', dragMode.start.bind(this));
+                this._listeners.dragModeEnd = addListener(this.canvas, 'mouseup', dragMode.end.bind(this));
             },
             teardown: function () {
-                console.log('teardown drawing');
+                this._listeners.dragModeStart.remove();
+                this._listeners.dragModeEnd.remove();
             }
         }
     };
