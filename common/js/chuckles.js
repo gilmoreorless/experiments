5da4461e720bbbad13cf60ec00d9a1003d8c65f9
Gilmore Davidson <gilmoreorless@gmail.com>
2012-11-07 17:48:59 +1100

    New drawing mode for Chuckles to define segment path on-the-fly

--- common/js/chuckles.js
+++ common/js/chuckles.js
@@ -28,7 +28,7 @@ OPTIMISATIONS
 
 * Don't constantly render to one canvas, use stacked canvases instead
     * Main canvas gets drawn once only, with bg image minus cut-away segment
-    * New canvas on top only gets segment, aloowing for faster redraws
+    * New canvas on top only gets segment, allowing for faster redraws
 
 *****/
 
@@ -57,6 +57,8 @@ Chuckles = (function () {
             this.canvas = document.createElement('canvas');
         }
         this.ctx = this.canvas.getContext('2d');
+        this.mode = 'normal';
+        this._listeners = {};
 
         this.movement = options.movement || {x: 0, y: 0};
         this.steps = +options.steps || 0;
@@ -86,6 +88,9 @@ Chuckles = (function () {
     };
 
     cproto._drawSegment = function () {
+        if (this.segmentPath.length < 2) {
+            return;
+        }
         var ctx = this.ctx;
         // Clear the relative part of the bg image
         ctx.save();
@@ -127,6 +132,101 @@ Chuckles = (function () {
         }
     };
 
+    function addListener(elem, type, handler) {
+        if (elem.addEventListener) {
+            elem.addEventListener(type, handler, false);
+        } else if (elem.attachEvent) {
+            elem.attachEvent('on' + type, handler);
+        }
+        return {
+            remove: function () {
+                removeListener(elem, type, handler);
+            }
+        };
+    }
+
+    function removeListener(elem, type, handler) {
+        if (elem.removeEventListener) {
+            elem.removeEventListener(type, handler, false);
+        } else if (elem.detachEvent) {
+            elem.detachEvent('on' + type, handler);
+        }
+    }
+
+    function preventDefault(e) {
+        if (e.preventDefault) {
+            e.preventDefault();
+        } else {
+            e.returnValue = false;
+        }
+    }
+
+    function getXY(e) {
+        return {
+            x: e.offsetX,
+            y: e.offsetY
+        };
+    }
+
+    var drawMode = {
+        start: function (e) {
+            var xy = getXY(e);
+            this._points = [xy];
+            this._tmpPath = [['moveTo', xy.x, xy.y], ['lineTo']];
+            this._listeners.drawModeDraw = addListener(this.canvas, 'mousemove', drawMode.draw.bind(this));
+            this._listeners.selectStart = addListener(document, 'selectstart', preventDefault);
+        },
+        draw: function (e) {
+            var xy = getXY(e);
+            this._points.push(xy);
+            this._tmpPath[1].push([xy.x, xy.y]);
+            drawMode.render(this);
+        },
+        end: function (e) {
+            this._listeners.drawModeDraw.remove();
+            this._listeners.selectStart.remove();
+            // TODO: Use Simplify.js to reduce _points, then use that to make a new _tmpPath
+            this._tmpPath.push(['closePath']);
+            drawMode.render(this);
+            this.setSegmentPath(this._tmpPath);
+            delete this._points;
+            delete this._tmpPath;
+        },
+        render: function (chuck) {
+            chuck.render();
+            chuck.ctx.save();
+            chuck.ctx.strokeStyle = '#000';
+            chuck._drawPath(chuck._tmpPath);
+            chuck.ctx.stroke();
+            chuck.ctx.restore();
+        }
+    };
+
+    var validModes = {normal: 1, drawing: 1, dragging: 1};
+    var modeEvents = {
+        drawing: {
+            setup: function () {
+                this.setSegmentPath([]);
+                this.setPosition(0);
+                // TODO: Function.prototype.bind shim
+                this._listeners.drawModeStart = addListener(this.canvas, 'mousedown', drawMode.start.bind(this));
+                this._listeners.drawModeEnd = addListener(this.canvas, 'mouseup', drawMode.end.bind(this));
+            },
+            teardown: function () {
+                this._listeners.drawModeStart.remove();
+                this._listeners.drawModeEnd.remove();
+            }
+        },
+        dragging: {
+            setup: function () {
+                console.log('setup drawing');
+            },
+            teardown: function () {
+                console.log('teardown drawing');
+            }
+        }
+    };
+
     /*** Public: Actions ***/
 
     cproto.render = function () {
@@ -155,7 +255,6 @@ Chuckles = (function () {
     };
 
     cproto.setSegmentPath = function (path) {
-        // TODO: How is path formatted?
         this.segmentPath = path;
     };
 
@@ -165,6 +264,21 @@ Chuckles = (function () {
         }
         this.position = position;
         this.render();
+    };
+
+    cproto.setMode = function (mode) {
+        if (!validModes.hasOwnProperty(mode)) {
+            return;
+        }
+        var oldModeEvent = (modeEvents[this.mode] || {}).teardown;
+        var newModeEvent = (modeEvents[mode] || {}).setup;
+        if (oldModeEvent) {
+            oldModeEvent.call(this);
+        }
+        this.mode = mode;
+        if (newModeEvent) {
+            newModeEvent.call(this);
+        }
     };
 
     /*** Public: Getters ***/
