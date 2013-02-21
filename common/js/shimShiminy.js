744a8977b00f5705933e3eb7f177d6784abe0f9a
Gilmore Davidson <gilmoreorless@gmail.com>
2013-02-21 12:57:22 +1100

    Google Analytics for all experiments

--- common/js/shimShiminy.js
+++ common/js/shimShiminy.js
@@ -51,3 +51,18 @@
                                       return clearTimeout(id);
                                   };
 })(this);
+
+/**
+ * Analytics - might as well put this here since this file is included everywhere anyway
+ */
+var _gaq = _gaq || [];
+if (~location.hostname.indexOf('github.com')) {
+    _gaq.push(['_setAccount', 'UA-8341018-3']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+}
