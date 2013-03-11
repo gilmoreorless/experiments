1c99f23ca88c6ebec81f655b87861e2e87b030df
Gilmore Davidson <gilmoreorless@gmail.com>
2013-03-12 08:39:17 +1100

    Upgraded Chuckles to latest version

--- common/js/chuckles.js
+++ common/js/chuckles.js
@@ -1,10 +1,15 @@
+/*!
+ * Chuckles
+ * https://github.com/gilmoreorless/chuckles
+ * Open source under the MIT licence: http://gilmoreorless.mit-license.org/
+ */
+
 /*****
 
-NOTES:
+FEATURES:
 
-* Canvas has a single background image (e.g. face)
+√ Canvas has a single background image (e.g. face)
     √ Image can be a string src, Image element or callback function for drawing to canvas directly
-    * Allow offset positioning on canvas, don't default to 0,0
 √ Segment is cut away from bg image (e.g. mouth)
     √ NEED DEFINITION OF SEGMENT PATH - series of context commands?
 √ Movement is where the path should move to at 100%
@@ -16,20 +21,6 @@ NOTES:
 √ Bind an input source to position for auto-updating
     √ HTML input element (text|range|radio|checkbox, textarea, select)
     √ AudioContext for sound-based
-
-FUTURE IDEAS
-
-* Movement options: Easing, keyframes, transforms (rotate, scale, skew)
-* Support multiple segments
-    * Idea 1: All segments behave the same way, based on a single position
-    * Idea 2: New segment types with different behaviour and different positions
-              (e.g. eyes move left-right while mouth moves up-down)
-
-OPTIMISATIONS
-
-* Don't constantly render to one canvas, use stacked canvases instead
-    * Main canvas gets drawn once only, with bg image minus cut-away segment
-    * New canvas on top only gets segment, allowing for faster redraws
 
 *****/
 
@@ -78,7 +69,8 @@ Chuckles = (function () {
 
     /*** Internal methods ***/
 
-    cproto._drawBackground = function (callback) {
+    cproto._drawBackground = function () {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         if (typeof this.image === 'function') {
             this.ctx.save();
             this.image(this.ctx);
